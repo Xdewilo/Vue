b8cc43c6c9fc23c2040ae35811206a1e829f2fb3
jeremy <jeremyposada2003@gmail.com>
2024-05-24 09:10:36 -0500

    primer hola mundo

---
+++ src/components/EjemploComponents.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-if="age < 10">Hola mundo</div>
+    <div v-else>Hola mundo 2</div>
+</template>
+
+a 
+<script lang="ts" setup>
+    let age = 10
+</script>
+
+
+<style scoped>
+
+</style>
