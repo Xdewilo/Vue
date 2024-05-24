00acb2aec13433709c95c05ee550b34a211a7940
jeremy <jeremyposada2003@gmail.com>
2024-05-24 11:34:22 -0500

    creando classBind

---
+++ src/components/ClassBind.vue
@@ -0,0 +1,20 @@
+<template>
+    <p v-bind:class="{'danger': hasError}"> El mensaje enviado con Exito</p>
+</template>
+
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'ClassBind',
+        setup(){
+            let hasError = false;
+        }
+    })
+</script>
+
+<style scoped>
+    .danger{
+        background-color: red;
+    }
+</style>
