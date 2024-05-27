c7880f2ac13619356fbb97bfa171ca9f15cc29b5
jeremy <jeremyposada2003@gmail.com>
2024-05-27 14:50:41 -0500

    cycle of life

---
+++ components-communication-life-cycle/src/components/CycleComposition.vue
@@ -0,0 +1,20 @@
+<template >
+  <div>
+    
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, onUpdated } from "vue";
+
+onMounted(() => {
+  console.log('mounted')
+})
+,
+onUpdated(() => {
+  console.log('Updated')
+})
+
+</script>
+<style scoped>
+  
+</style>
---
+++ components-communication-life-cycle/src/components/CycleOpcion.vue
@@ -0,0 +1,31 @@
+<template >
+  <div>
+    Hola mundo
+  </div>
+</template>
+
+
+<script lang="ts">
+//             !OPTION API¡
+import { defineComponent} from "vue";
+ export default defineComponent({
+    name:'CycleOpcion',
+    created() {
+        console.log('create')
+    },
+    mounted() {
+        console.log('montado')
+    },
+    updated() {
+        console.log('updated')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+ })
+
+</script>
+
+<style scoped>
+  
+</style>
