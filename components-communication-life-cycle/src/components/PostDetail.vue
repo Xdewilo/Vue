db349e6aafa59f2e1aede835ca2b2d560077f904
jeremy <jeremyposada2003@gmail.com>
2024-05-27 14:50:55 -0500

    cominicacion vertical

---
+++ components-communication-life-cycle/src/components/PostDetail.vue
@@ -0,0 +1,44 @@
+<template >
+
+  <div class="card">
+    <h3>{{ props.title }}</h3>
+    <p>{{ props.content }}</p>
+  </div>
+
+</template>
+
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name: 'PostDetail',
+    props: {
+        title : {
+            type: String,
+            required: true,
+            default : 'no tiene titulo'
+        },
+        content :{
+            type : String,
+            required : false,
+            default : "no tiene contenido"
+        }
+    },
+    setup(props){
+        return {props}
+    }
+})
+</script>
+
+
+<style scoped>
+  .card{
+    width: 500px;
+    height: 100px;
+    background-color: blue;
+    color: antiquewhite;
+    border-radius: 20px;
+    margin: 10px;
+    padding: 10px;
+  }
+</style>
---
+++ components-communication-life-cycle/src/components/PostList.vue
@@ -0,0 +1,50 @@
+<!-- PostList.vue -->
+<template>
+  <div class="container">
+    <PostDetail v-for="item in info" :key="item.title" :titulo="item.title" :contenido="item.content" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PostDetail from './PostDetail.vue';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+const info: Array<Post> = [
+  {
+    title: 'Este es el post 1',
+    content: 'contenido 1',
+  },
+  {
+    title: 'Este es el post 2',
+    content: 'contenido 2',
+  },
+  {
+    title: 'Este es el post 3',
+    content: 'contenido 3',
+  },
+  {
+    title: 'Este es el post 4',
+    content: 'contenido 4',
+  },
+  {
+    title: 'Este es el post 5',
+    content: 'contenido 5',
+  },
+  {
+    title: 'Este es el post 6',
+    content: 'contenido 6',
+  },
+];
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+</style>
