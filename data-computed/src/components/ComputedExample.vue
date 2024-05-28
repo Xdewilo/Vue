27bf2fb83055974e60e5349d85ee4b2f51998afa
jeremy <jeremyposada2003@gmail.com>
2024-05-28 14:54:54 -0500

    hola

---
+++ data-computed/src/components/ComputedExample.vue
@@ -0,0 +1,22 @@
+<template >
+  <div>
+    <input type="text"  v-model="msg"/>
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+<script lang="ts">
+//option API
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name: 'ComputedExample',
+    data() {
+        return {
+            msg : ''
+        }
+    },
+})
+
+</script>
+<style scoped>
+  
+</style>
