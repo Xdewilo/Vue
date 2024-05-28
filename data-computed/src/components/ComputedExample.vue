87a922662d31a73dd27fceaeac076e9d616e6a74
jeremy <jeremyposada2003@gmail.com>
2024-05-28 15:10:55 -0500

    fin computed

--- data-computed/src/components/ComputedExample.vue
+++ data-computed/src/components/ComputedExample.vue
@@ -2,10 +2,13 @@
   <div>
     <input type="text"  v-model="msg"/>
     <h2>{{ msg }}</h2>
+    <h3>{{ reverse }}</h3>
   </div>
 </template>
-<script lang="ts">
-//option API
+
+
+<!--<script lang="ts">
+        //OPTION API
 import {defineComponent} from 'vue'
 export default defineComponent({
     name: 'ComputedExample',
@@ -14,9 +17,39 @@ export default defineComponent({
             msg : ''
         }
     },
+    computed: {
+        reverse() : string{
+            return this.msg.split(' ').reverse().join(' ')
+        }
+    }
 })
 
+</script>-->
+
+<!--<script lang="ts">
+//              Composicion API
+import { defineComponent, ref, Ref, computed } from 'vue'
+export default defineComponent({
+    name: 'ComputedExample',
+    setup(){
+        let msg : Ref<string>= ref('')
+        const reverse = computed(() => msg.value.split('').reverse().join(''))
+        return {msg, reverse}
+    }
+})
 </script>
+<style scoped>
+-->
+
+
+
+<script lang="ts" setup>
+//              Composicion API (setup)
+import { ref, Ref, computed } from 'vue'
+    let msg : Ref<string>= ref('')
+    const reverse = computed(() => msg.value.split('').reverse().join(''))
+</script>
+
 <style scoped>
   
 </style>
