ed92069a2c476552c80fb6c44ba52329218f7975
jeremy <jeremyposada2003@gmail.com>
2024-05-29 11:57:33 -0500

    code documentado

--- ejercicio2/src/App.vue
+++ ejercicio2/src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <!-- Contenedor principal -->
   <div>
+    <!-- Componente Ejercicio2Components -->
     <Ejercicio2Components/>
   </div>
 </template>
 
 <script lang="ts" setup>
- import Ejercicio2Components from './components/Ejercicio2Components.vue';
+  // Importa el componente Ejercicio2Components desde el archivo especificado
+  import Ejercicio2Components from './components/Ejercicio2Components.vue';
 </script>
 
+<!-- Estilos para el componente -->
 <style>
-
 </style>
--- ejercicio2/src/components/Ejercicio2Components.vue
+++ ejercicio2/src/components/Ejercicio2Components.vue
@@ -1,25 +1,39 @@
 <template>
-  <div>
-  <label >Subtotal</label>
-    <input type="text" v-model="subtotal" >
-  </div>
-  <h3>Total impuesto a pagar {{ totalVat }} ({{ vat }})</h3>
+    <div>
+      <!-- Etiqueta para el campo de texto del subtotal -->
+      <label>Subtotal</label>
+      <!-- Campo de texto con enlace bidireccional al valor 'subtotal' -->
+      <input type="text" v-model="subtotal">
+    </div>
+    <!-- Muestra el total del IVA a pagar calculado y el porcentaje del IVA -->
+    <h3>Total impuesto a pagar {{ totalVat }} ({{ vat }}%)</h3>
 </template>
-
+  
 <script lang="ts" setup>
-import { Ref, ref, computed } from "vue";
-
-const vat = 21;
-let subtotal: Ref<number> = ref(0); 
-const totalVat = computed(() =>(vat *subtotal.value)/ 100 )
-
-/*
-
-100 -------------- subtotal
-21 ---------------- X
-x 0 (21 * subtotal)/ 100
-
-*/
+    // Importación de funcionalidades de Vue
+    import { Ref, ref, computed } from "vue";
+  
+    // Definición de la tasa de IVA (21%)
+    const vat = 21;
+  
+    // Definición del valor reactivo 'subtotal' para almacenar el valor del subtotal
+    let subtotal: Ref<number> = ref(0);
+  
+    // Definición de un valor computado 'totalVat' que calcula el IVA total a pagar
+    const totalVat = computed(() => (vat * subtotal.value) / 100);
+  
+    /*
+    Fórmula para el cálculo del IVA:
+    
+    Si el subtotal es 100, el IVA es 21. Se calcula con la fórmula:
+    (21 * subtotal) / 100
+  
+    Ejemplo:
+    Si el subtotal es 200:
+    Total IVA = (21 * 200) / 100 = 42
+    */
 </script>
-
-<style scoped></style>
+  
+  <!-- Estilos scoped (limitados a este componente) -->
+<style scoped>
+</style>
