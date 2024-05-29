192edbe9b45cb26e175984d9fc4896ccf67e97bc
jeremy <jeremyposada2003@gmail.com>
2024-05-29 11:53:46 -0500

    ejercicio resuelto

--- ejercicio2/src/App.vue
+++ ejercicio2/src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <Ejercicio2Components/>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script lang="ts" setup>
+ import Ejercicio2Components from './components/Ejercicio2Components.vue';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ ejercicio2/src/components/Ejercicio2Components.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <label >Subtotal</label>
+    <input type="text" v-model="subtotal" >
+  </div>
+  <h3>Total impuesto a pagar {{ totalVat }} ({{ vat }})</h3>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref, computed } from "vue";
+
+const vat = 21;
+let subtotal: Ref<number> = ref(0); 
+const totalVat = computed(() =>(vat *subtotal.value)/ 100 )
+
+/*
+
+100 -------------- subtotal
+21 ---------------- X
+x 0 (21 * subtotal)/ 100
+
+*/
+</script>
+
+<style scoped></style>
