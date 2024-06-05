d40cf8e5ab56b9ae0b3072b738c3f68688ff53bc
jeremy <jeremyposada2003@gmail.com>
2024-06-05 09:31:20 -0500

    creeate View de Post

---
+++ enrutado/src/views/BlogView.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Blog View</h1>
+    <ul>
+        <li><router-link :to="{name: 'post', params: {id: 1}}" >Primer Post</router-link></li>
+        <li><router-link :to="{name: 'post', params: {id: 2}}" >Segundo Post</router-link></li>
+        <li><router-link :to="{name: 'post', params: {id: 3}}" >Tercer Post</router-link></li>
+    </ul>
+</template>
---
+++ enrutado/src/views/PostView.vue
@@ -0,0 +1,15 @@
+<template >
+  <h1>Post View del elemeto {{ postId }}</h1>
+
+</template>
+
+<script lang="ts" setup>
+    import {onMounted , ref} from 'vue'
+    import {useRoute} from 'vue-router'
+
+    let postId = ref('')
+    onMounted(() => {
+       const route = useRoute()
+       postId.value = route.params.id
+    })
+</script>
