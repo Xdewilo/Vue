e2928662b24d9cba29fd6a0fa3c689d6276a7611
jeremy <jeremyposada2003@gmail.com>
2024-06-09 12:37:20 -0500

    create view

---
+++ estructura-de-auntentificacion/src/views/AuthView.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>This is an Auth </h1>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email" v-model="email">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  const email = ref('')
+  const password = ref('')
+
+</script>
