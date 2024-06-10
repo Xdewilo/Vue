38129ef3d3ab47654cafa3b760d7a4419a0ce21e
jeremy <jeremyposada2003@gmail.com>
2024-06-10 17:11:25 -0500

    login successs

--- estructura-de-auntentificacion/src/views/AuthView.vue
+++ estructura-de-auntentificacion/src/views/AuthView.vue
@@ -10,19 +10,30 @@
         <label for="password">Password</label>
         <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary" @click="loginUser()">Submit</button>
     </form>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
-  const email = ref('')
-  const password = ref('')
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import AuthService from '@/services/AuthService';
+
+const email = ref('')
+const password = ref('')
+
+const router = useRouter()
+
+const loginUser = async () => {
+  const authService = new AuthService()
+  const success = await authService.login(email.value, password.value)
+
+  if (success) {
+    alert('Login Success')
+  } else {
+    alert('Login Failed')
+  }
+}
 
 </script>
-
-https://hfp69ilv.directus.app/auth/login
-
-email. testuser@jeremy.net
-password: 12345678
