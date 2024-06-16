9172c022c412e16ddb885ddb54b37e4bbd16e6ca
jeremy <jeremyposada2003@gmail.com>
2024-06-16 12:05:23 -0500

    crate FireBase project and install bootstrap

---
+++ firebase/src/views/FireBase.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>FireBase Login</h1>
+    
+</template>
---
+++ firebase/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Home auth</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scope>
+</style>
