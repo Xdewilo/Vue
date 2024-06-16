d92be17e9b0136937ebce9a2ef890bc33df3feb4
jeremy <jeremyposada2003@gmail.com>
2024-06-16 12:19:25 -0500

    firebase terminado

--- firebase/src/main.ts
+++ firebase/src/main.ts
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import { initializeApp } from 'firebase/app'; 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAcCMeWr8vyOipy1sE2VraurUzg4pLHNy0",
+    authDomain: "curso-vue-f45f1.firebaseapp.com",
+    projectId: "curso-vue-f45f1",
+    storageBucket: "curso-vue-f45f1.appspot.com",
+    messagingSenderId: "367100754137",
+    appId: "1:367100754137:web:eda83528e35382bc0726c8",
+    measurementId: "G-QMVG4VPG3X"
+  };
 
-
+initializeApp(firebaseConfig);
 createApp(App).use(router).mount('#app')
--- firebase/src/views/FireBase.vue
+++ firebase/src/views/FireBase.vue
@@ -1,4 +1,36 @@
+<!-- src/components/LoginForm.vue -->
 <template>
+  <div class="container">
     <h1>FireBase Login</h1>
-    
+    <form @submit.prevent="loginUser">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email" required class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required class="form-control" />
+      </div>
+      <button type="submit" class="btn btn-primary mt-3">Login</button>
+    </form>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+ 
+const email = ref('');
+const password = ref('');
+
+const loginUser = async () => {
+  const auth = getAuth();
+  try {
+    await signInWithEmailAndPassword(auth, email.value, password.value);
+    alert('User logged in');
+  } catch (error) {
+    alert('Error logging in');
+  }
+};
+
+</script>
