ab6afdb4a83eefb54347ffadc8ddc9860c5cb545
jeremy <jeremyposada2003@gmail.com>
2024-06-16 14:20:20 -0500

    modify firebase

--- firebase/src/App.vue
+++ firebase/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">FireBase</router-link>
+    <router-link to="/firebase">FireBase</router-link>
   </nav>
   <router-view/>
 </template>
