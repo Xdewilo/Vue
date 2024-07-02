839752fe3a9d5110f735512ae6920c0601950daf
jeremy <jeremyposada2003@gmail.com>
2024-07-02 11:40:48 -0500

    listo

--- cookies_and_sesions/src/main.ts
+++ cookies_and_sesions/src/main.ts
@@ -1,5 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueCookies from 'vue-cookies'
+//npm install vue-cookies
+import VueCookies from 'vue-cookies' 
+//npm install vue-session
+import VueSession from 'vue-session'
 
-createApp(App).mount('#app')
+const app = createApp(App)  
+app.use(VueCookies, {
+    // config
+    expireTimes: "1d",
+})
+.use(VueSession).mount('#app')
+
+
+// cookies.set('auth' , 1000 <== value)
+// cookies.get('auth') 
+// cookies.remove('auth')
+// cookies.isKey('auth')
+// cookies.keys()
+
+
+//session.start()
+//session.set('auth', 1000)
+//session.get('auth')
+//session.id()
+//session.remove('auth')
+//session.destroy()
+
