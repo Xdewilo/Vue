9ed2019a295d42a02e2aa96e2890e4d350d9289c
jeremy <jeremyposada2003@gmail.com>
2024-07-08 16:02:20 -0500

    auth terminado

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -1,7 +1,6 @@
-import {defineStore} from 'pinia'  
-import {Usuario} from '../Interface/Usuario'  
+import {defineStore} from 'pinia'  // Import the defineStore function from the pinia package
 
-
+// Define a new store called useAuth
 const useAuth = defineStore('aunth', {
     state: () => {
         return {
@@ -9,11 +8,11 @@ const useAuth = defineStore('aunth', {
             baseURL: 'http://127.0.0.1:8000/api'
         }
     },
-
+    // Define the actions property
     actions: {
-        async login(name : string, email : string, password: string){
+        async login(email : string, password: string){
 
-            const data = await fetch(`${this.baseURL}/auth/register`, {
+            const data = await fetch(`${this.baseURL}/auth/login`, {
                 method: 'POST',
 
                 headers: {
@@ -21,32 +20,46 @@ const useAuth = defineStore('aunth', {
                     'Accept': 'application/json'
                 },
 
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            })
+            const response = await data.json()
+            if(response.status == false ){
+                this.token = null
+                return false
+            }else{
+                this.token = response.token
+                return true
+            }
+        },
+
+
+        async register(name:string, email:string, password:string){
+            const data = await fetch(`${this.baseURL}/auth/register`, {
+
+                method: 'POST',
+
+                headers: {
+
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
                 body: JSON.stringify({
                     name,
                     email,
                     password
                 })
             })
+            
             const response = await data.json()
-        },
-
-
-        async register(email:string, password:string){
-            const data = await fetch(`${this.baseURL}/auth/register`, {
-
-                method: 'POST',
-
-                headers: {
-
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            })
-            const response = await data.json()
+            if(response.status === false){
+                return false
+            }else{
+                this.token = response.token
+                return true
+            }
         },
         async getNotes(){
             const data = await fetch(`${this.baseURL}/note`, {
@@ -76,6 +89,11 @@ const useAuth = defineStore('aunth', {
             })
 
             const response = await data.json()
+            if(response.status === false){
+                return false
+            }else{
+                return true
+            }
             
         }
     }   
