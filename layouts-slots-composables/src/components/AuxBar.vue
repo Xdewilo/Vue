abe9ed3ee8a4addafbdd28fd7aa40531dcdf8983
jeremy <jeremyposada2003@gmail.com>
2024-07-09 09:56:41 -0500

    aux bar

---
+++ layouts-slots-composables/src/components/AuxBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/service">Service</router-link> |
+            <router-link to="/contact">Contact</router-link>
+        </nav>
+        <router-view/>
+    </div>
+</template>
