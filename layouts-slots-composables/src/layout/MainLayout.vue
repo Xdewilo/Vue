3e9139cafbb49f018b5cabc85cb1fed53bdc658a
jeremy <jeremyposada2003@gmail.com>
2024-07-09 09:57:08 -0500

    main layout

---
+++ layouts-slots-composables/src/layout/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+      <div class="auxbar">
+        <AuxBar />
+      </div>
+  
+      <div class="main-content-container">
+        <div class="men">
+          <MenuSideBar />
+        </div>
+  
+        <div class="main-content">
+            <slot name="main"></slot>
+        </div>
+        <div class="aux-content">
+            <slot name="aux"></slot>
+        </div>
+
+
+      </div>
+    </div>
+</template>
+  
+<script lang="ts" setup>
+import MenuSideBar from '@/components/MenuSideBar.vue';
+import AuxBar from '@/components/AuxBar.vue';
+
+  
+</script>
+  
