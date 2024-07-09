d347b3df5cc536ccba40d22f30f7f02c1f6bb1e1
jeremy <jeremyposada2003@gmail.com>
2024-07-09 09:57:13 -0500

    index

--- layouts-slots-composables/src/router/index.ts
+++ layouts-slots-composables/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import HomeView from '@/views/HomeView.vue'
+import ContactView from '@/views/ContactView.vue'
+import ServiceView from '@/views/ServiceView.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -14,6 +15,16 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+  {
+    path: '/service',
+    name: 'service',
+    component: ServiceView
   }
 ]
 
