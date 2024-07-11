97ee31d65c97cdad9a5687e8c138dc2ce0718826
jeremy <jeremyposada2003@gmail.com>
2024-07-11 10:22:12 -0500

    TableLayout

--- componentes-dinamicos/src/components/layout/TableLayout.vue
+++ componentes-dinamicos/src/components/layout/TableLayout.vue
@@ -1,9 +1,15 @@
 <template >
   <div>
     <h1>table layout</h1>
+    <UserRow v-for="item in data.content" :key="item.name" :content="item" />
+
   </div>
 </template>
 <script lang="ts" setup>
+import { defineProps } from 'vue'
+import UserRow from '../UserRow.vue';
+
+const data = defineProps(['content'])
 
 </script>
 <style scoped>
