8357262bf518bf89ffe512bf4287d602ce1da9e3
jeremy <jeremyposada2003@gmail.com>
2024-07-11 15:23:42 -0500

    main

--- trasition/src/main.ts
+++ trasition/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import{ BiFileEarmarkFontFill} from 'oh-vue-icons/icons'
 
-createApp(App).use(router).mount('#app')
+addIcons(BiFileEarmarkFontFill)
+
+
+const app = createApp(App)
+app.component('v-icon',OhVueIcon).use(router)
+
+app.mount('#app')
