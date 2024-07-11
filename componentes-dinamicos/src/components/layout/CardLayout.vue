cb20aa148343151f9d66a44b994638e02377df7e
jeremy <jeremyposada2003@gmail.com>
2024-07-11 10:21:39 -0500

    layoutCard

--- componentes-dinamicos/src/components/layout/CardLayout.vue
+++ componentes-dinamicos/src/components/layout/CardLayout.vue
@@ -1,12 +1,39 @@
-<template >
+<template>
   <div>
-      <button>Ver en tarjetas</button>
-    
+    <h1>Ver en tarjetas</h1>
+    <div class="container">
+      <UserCard v-for="item in props.content" :key="item.name" :content="item" />
+      {{ props.content.name }}
+      {{ props.content.age }}
+      {{ props.content.position }}
+      
+    </div>
+
   </div>
 </template>
-<script lang="ts" setup>
 
+<script lang="ts" setup>
+import UserCard from '../UserCard.vue';
+import { defineProps } from 'vue'
+
+const props = defineProps(['content'])
 </script>
+
+
+
 <style scoped>
-  
+.container {
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 1rem;
+  padding: 1rem;
+  margin: 1rem;
+  justify-content: center;
+  align-items: center;
+  justify-items: center;
+  align-content: center;
+  justify-self: center;
+}
+
 </style>
