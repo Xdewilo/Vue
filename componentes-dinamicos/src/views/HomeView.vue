49a3dfb75d64e4b6d4330d7af102c7cf3c077869
jeremy <jeremyposada2003@gmail.com>
2024-07-11 10:22:21 -0500

    HomeView

--- componentes-dinamicos/src/views/HomeView.vue
+++ componentes-dinamicos/src/views/HomeView.vue
@@ -1,18 +1,60 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<template >
+  <div>
+    <h1>Listado ded Usuarios</h1>
+      <button @click="handlerLayout(list)">Ver en lista </button>
+      <button @click="handlerLayout(card)">Ver en tarjetas</button>
+      <button @click="handlerLayout(table)">Ver en tabla </button>
   </div>
+  <component :is="layout" :content="users"/> 
+
 </template>
+<script lang="ts" setup>
+import { ref, defineAsyncComponent } from 'vue';
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+const list = defineAsyncComponent(() => import('@/components/layout/ListLayout.vue')) 
+const card = defineAsyncComponent(() => import('@/components/layout/CardLayout.vue'))
+const table = defineAsyncComponent(() => import('@/components/layout/TableLayout.vue'))
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+const layout = ref(list)
+
+const handlerLayout = (cmd : any) => layout.value = cmd
+
+
+const users = ref([
+{
+  name : 'Juan',
+  age : 20,
+  position : 'Developer'
+},
+{
+  name : 'Pedro',
+  age : 30,
+  position : 'Designer'
+},
+{
+  name : 'Maria',
+  age : 25,
+  position : 'Developer'
+},
+{
+  name : 'Ana',
+  age : 35,
+  position : 'Designer'
+},
+{
+  name : 'Luis',
+  age : 40,
+  position : 'Developer'
+},
+{
+  name : 'Carlos',
+  age : 45,
+  position : 'Designer'
+
+}])
+
+
 </script>
+<style scoped>
+  
+</style>
