1762a45c10deea338894867777d361c9c75a203e
jeremy <jeremyposada2003@gmail.com>
2024-07-11 10:57:06 -0500

    listo pues

--- componentes-dinamicos/src/views/HomeView.vue
+++ componentes-dinamicos/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template >
   <div>
+  <input type="text" placeholder="Filtar Usuario" v-model="search" @keyup="handlerSearch">
     <h1>Listado ded Usuarios</h1>
       <button @click="handlerLayout(list)">Ver en lista </button>
       <button @click="handlerLayout(card)">Ver en tarjetas</button>
       <button @click="handlerLayout(table)">Ver en tabla </button>
   </div>
-  <component :is="layout" :content="users"/> 
+  <component :is="layout" :content="filterUsers"/> 
 
 </template>
 <script lang="ts" setup>
@@ -15,7 +16,14 @@ const list = defineAsyncComponent(() => import('@/components/layout/ListLayout.v
 const card = defineAsyncComponent(() => import('@/components/layout/CardLayout.vue'))
 const table = defineAsyncComponent(() => import('@/components/layout/TableLayout.vue'))
 
+const search = ref('')
 const layout = ref(list)
+
+
+const handlerSearch = () => {
+  filterUsers.value = users.value.filter(user => user.name.toLowerCase().includes(search.value.toLowerCase()))
+}
+
 
 const handlerLayout = (cmd : any) => layout.value = cmd
 
@@ -50,9 +58,10 @@ const users = ref([
   name : 'Carlos',
   age : 45,
   position : 'Designer'
-
+  
 }])
 
+const filterUsers = ref(users.value)
 
 </script>
 <style scoped>
