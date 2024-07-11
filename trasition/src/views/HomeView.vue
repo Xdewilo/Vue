b6595becd51e6f3c4282dbe64f549109f5f1c9ca
jeremy <jeremyposada2003@gmail.com>
2024-07-11 15:24:01 -0500

    home

--- trasition/src/views/HomeView.vue
+++ trasition/src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ListComponents/>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" >
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ListComponents from '@/components/ListComponents.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
+    ListComponents,
   },
 });
 </script>
