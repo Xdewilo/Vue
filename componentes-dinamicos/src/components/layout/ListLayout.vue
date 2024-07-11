408732260813cfc5c7ae5c537340c045b9099896
jeremy <jeremyposada2003@gmail.com>
2024-07-11 10:22:04 -0500

    ListLayout

--- componentes-dinamicos/src/components/layout/ListLayout.vue
+++ componentes-dinamicos/src/components/layout/ListLayout.vue
@@ -1,10 +1,16 @@
 <template >
   <div>
-      <button>Ver en lista </button>
+      <h1>Ver en lista </h1>
+      <UserList v-for="item in data.content" :key="item.name" :content="item" />
     
   </div>
 </template>
 <script lang="ts" setup>
+
+import { defineProps } from 'vue'
+import UserList from '../UserList.vue';
+
+const data = defineProps(['content'])
 
 </script>
 <style scoped>
