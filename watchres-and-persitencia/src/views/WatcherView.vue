64332ef5bd37236564567222f44266f6d4787f93
jeremy <jeremyposada2003@email.com>
2024-07-24 15:55:34 -0500

    vue

---
+++ watchres-and-persitencia/src/views/WatcherView.vue
@@ -0,0 +1,25 @@
+<template >
+  <h1>Notes</h1>
+  <input type="text" placeholder="Digite note" v-model="note">
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+export default defineComponent({
+  data(){
+    return {
+        note :''
+    }
+  },
+    watch:{
+        note(newVal, oldVal){
+        alert("Valor nuevo ==>"+ newVal + "Valor antiguao ===>" +oldVal)
+        localStorage.setItem('note', newVal)
+        }
+    }
+})
+
+</script>
+<style scoped>
+  
+</style>
