c3a49c1afb1d07fc4dd7ef5cb7d9331171cd2a29
jeremy <jeremyposada2003@email.com>
2024-07-25 11:10:29 -0500

    Revert "create child and parent"
    This reverts commit 54facaada8e76ae08de9a8c3b237b6f47e578fb7.

--- control-de-legado-v-lazy-and-webchunk/src/views/HomeView.vue
+++ control-de-legado-v-lazy-and-webchunk/src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    <ParentCoponents />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import ParentCoponents from '@/components/ParentCoponents.vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    ParentCoponents,
+    HelloWorld,
   },
 });
 </script>
