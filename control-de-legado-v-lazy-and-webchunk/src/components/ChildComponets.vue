5df42ac3b5c9ed66139243b0dc301c789f96cb73
jeremy <jeremyposada2003@email.com>
2024-07-25 11:23:20 -0500

    vue

---
+++ control-de-legado-v-lazy-and-webchunk/src/components/ChildComponets.vue
@@ -0,0 +1,9 @@
+<template >
+    <h1>ChildComponets</h1>
+</template>
+<script lang="ts" setup>
+import { Ref,ref } from 'vue';
+</script>
+<style scoped>
+  
+</style>
---
+++ control-de-legado-v-lazy-and-webchunk/src/components/ParentCoponents.vue
@@ -0,0 +1,21 @@
+<template >
+
+    <h1>ParentCoponents</h1>
+
+    <ChildComponets />
+
+</template>
+
+<script lang="ts" setup>
+
+import ChildComponets from '@/components/ChildComponets.vue';
+
+
+
+</script>
+
+<style scoped>
+
+  
+
+</style>
