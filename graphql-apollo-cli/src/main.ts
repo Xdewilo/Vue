5c807f814eaa055d0b6dd92cb551e99eeff6e638
jeremy <jeremyposada2003@email.com>
2024-07-26 13:51:45 -0500

    config

--- graphql-apollo-cli/src/main.ts
+++ graphql-apollo-cli/src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {ApolloClient,createHttpLink,InMemoryCache} from '@apollo/client/core'
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql'
+
+});
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+});
 
 createApp(App).use(router).mount('#app')
