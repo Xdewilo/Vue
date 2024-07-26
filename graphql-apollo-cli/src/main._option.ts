46a2a9e6bb787e0e3d54b6a2e30fda56ee72329a
jeremy <jeremyposada2003@email.com>
2024-07-26 14:05:44 -0500

    finish config

--- graphql-apollo-cli/src/main._option.ts
+++ graphql-apollo-cli/src/main._option.ts
@@ -1,27 +1,32 @@
 import { createApp, provide, h } from 'vue'
 import App from './App.vue'
-import router from './router'
 import {ApolloClient,createHttpLink,InMemoryCache} from '@apollo/client/core'
-import { DefaultApolloClient} from '@vue/apollo-composable'
+import {createApolloProvider} from '@vue/apollo-option'
+import router from './router';
+
 // import { DefaultApolloClient } from '@vue/apollo-composable'
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000/graphql'
-
 });
+
+
 // Create the apollo client
 const cache = new InMemoryCache();
+
+
 // Create the apollo client
 const apolloClient = new ApolloClient({
-
     // Provide the apollo client to the app
     link: httpLink,
     cache
 });
 
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+    cliente2 : apolloClient2
+    cliente : apolloClient3
+});
 // Create a new app
 createApp({
-    setup() {
-        provide(DefaultApolloClient, apolloClient);
-    },
     render: () => h(App)
-}).use(router).mount('#app')
+}).use(apolloProvider).use(router).mount('#app')
